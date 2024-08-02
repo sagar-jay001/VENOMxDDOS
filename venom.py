0e963891c7b866c0fb57a57ec56f975ed4551893
Sagar Jaypuria <113926118+sagar-jay001@users.noreply.github.com>
2024-08-02 13:39:25 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '6707683692:AAHlcEjnHn2_FXVohVQCEJYyfod1NVAcq-4'
+ADMIN_ID = '1994270625'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
